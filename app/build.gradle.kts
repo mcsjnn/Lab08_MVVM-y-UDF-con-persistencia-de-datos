2d3500b6193a3f3e7a000842992aed560c260a5b
Sujenn Maciel Fiorella Ancco Escobar <sujenn.ancco@tecsup.edu.pe>
2024-09-23 14:04:36 -0500

    Se creo el modelo de datos con Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt") // Añadir esta línea
 }
 
 android {
@@ -49,6 +50,7 @@ android {
     }
 }
 
+val room_version = "2.6.1"
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -66,4 +68,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.room:room-runtime:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+// Agrega la dependencia de room-ktx para soporte de corrutinas
+    implementation("androidx.room:room-ktx:$room_version")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$room_version")
+
 }
